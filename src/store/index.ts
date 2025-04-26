8b1751f7e60b2d17396c1da112d507c548fcbf35
Harman Singh <98532264+H-SM@users.noreply.github.com>
2025-04-27 02:08:42 +0530

    fix: better parsing of place information, server-side pagination, better place details card (#6)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,25 @@
 import { create } from 'zustand';
 import { User, Place, UserPreferences } from '../types';
-
 interface AppState {
   user: User | null;
   places: Place[];
   selectedPlace: Place | null;
+  latitude: number | null;
+  longitude: number | null;
   userPreferences: UserPreferences;
   setUser: (user: User | null) => void;
   setPlaces: (places: Place[]) => void;
   setSelectedPlace: (place: Place | null) => void;
   setUserPreferences: (preferences: UserPreferences) => void;
+  setLocation: (latitude: number | null, longitude: number | null) => void;
 }
 
 export const useStore = create<AppState>((set) => ({
   user: null,
   places: [],
   selectedPlace: null,
+  latitude: null,
+  longitude: null,
   userPreferences: {
     nightlife: false,
     coffee: false,
@@ -28,4 +32,5 @@ export const useStore = create<AppState>((set) => ({
   setPlaces: (places) => set({ places }),
   setSelectedPlace: (place) => set({ selectedPlace: place }),
   setUserPreferences: (preferences) => set({ userPreferences: preferences }),
+  setLocation: (latitude, longitude) => set({ latitude, longitude }),
 }));
